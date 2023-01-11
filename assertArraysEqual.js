d392eb12fcfed0a6b41c4f17dc65ce7619de784f
Braden <braden.clokie@gmail.com>
2023-01-10 21:36:38 -0500

    Added assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } else {
+      return true;
+  }
+};
+}
+
+const assertArraysEqual = function(actual, expected) {
+  let assertion_result = eqArrays(actual, expected)
+  if (assertion_result) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1,2,3], [1,2,3])
+assertArraysEqual([1,2,3], [1,2,3,4])
+assertArraysEqual([1,2,3], [2,2,3])
+
