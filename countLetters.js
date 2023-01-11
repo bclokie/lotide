268f0a0e4f565e197a154e5a09f4cac58103b4d3
Braden <braden.clokie@gmail.com>
2023-01-11 16:46:23 -0500

    countLetters.js is not fully functional and I don't know why

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let letterCount = {};
+  for (let letter of sentence) {
+    if (letter !== " ") {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+  return letterCount;
+}
+
+
+assertEqual(countLetters('LHL'), {L:2,H:1});
+
+console.log(countLetters('lighthouse in the house'));
+
+// assertEqual is giving me Assertion Failed but I don't know why... Check back on this later
