93385e2149f2b1666ff26de1367e07395dd93b39
Braden <braden.clokie@gmail.com>
2023-01-11 19:45:42 -0500

    Added letterPositions function and singular assertArraysEqual check

---
+++ letterPositions.js
@@ -0,0 +1,37 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    } else {
+      return true;
+  }
+};
+}
+
+const assertArraysEqual = function(actual, expected) {
+  let assertion_result = eqArrays(actual, expected)
+  if (assertion_result) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual(letterPositions("hello").o, [4]);
