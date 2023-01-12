6bd0129c67fee4dc87da7feecbf68496d06d3d37
Braden <braden.clokie@gmail.com>
2023-01-11 20:23:40 -0500

    Created findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (list, show) => {
+  for (let genre in list) {
+    if (list[genre] === show) {
+      return genre;
+    }
+  }
+  return undefined;
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"))
