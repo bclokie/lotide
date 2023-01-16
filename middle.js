79ff71ca120016453e26ed99efe60367abf5aceb
Braden <braden.clokie@gmail.com>
2023-01-16 15:20:25 -0500

    Refactored middle function and added test function

--- middle.js
+++ middle.js
@@ -1,33 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    } else {
-      return true;
-  }
-};
-}
-
-const assertArraysEqual = function(actual, expected) {
-  let assertion_result = eqArrays(actual, expected)
-  if (assertion_result) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual')
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
 
 const middle = function(arr) {
   if (arr.length <= 2) {
@@ -41,16 +14,4 @@ const middle = function(arr) {
   }
 }
 
-console.log(middle([2, 3, 4, 5]))
-console.log(middle([ 2, 3, 4, 5, 6]))
-console.log(middle([1]))
-console.log(middle([1, 2]))
-console.log(middle([1, 2, 3]))
-
-
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
-assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
+module.exports = middle
---
+++ test/middleTest.js
@@ -0,0 +1,18 @@
+const assertEqual = require('../assertEqual')
+const eqArrays = require('../eqArrays')
+const assertArraysEqual = require('../assertArraysEqual')
+const middle = require('../middle');
+
+console.log(middle([2, 3, 4, 5]))
+console.log(middle([ 2, 3, 4, 5, 6]))
+console.log(middle([1]))
+console.log(middle([1, 2]))
+console.log(middle([1, 2, 3]))
+
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
+assertArraysEqual(middle([1, 2, 3, 4]),[2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]),[3, 4]);
