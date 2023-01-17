03f68263d950ba00d073c15b4843ad856cee6a93
Braden <braden.clokie@gmail.com>
2023-01-16 23:14:15 -0500

    updated typo in middle.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,7 @@
 const assert = require('chai').assert;
 const middle = require('../middle');
 
-describe("#tail", () => {
+describe("#middle", () => {
   it("returns [2] for [1, 2, 3]", () => {
     assert.deepEqual(middle([1, 2, 3]), [2]);
   });
